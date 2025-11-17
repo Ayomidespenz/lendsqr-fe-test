05ce7fde2756f9c77ab22dde667723b0656c9fd8
ayomide <quadriyusuff234@gmail.com>
2025-11-17 13:18:39 +0100

    fixed

--- src/services/userApi.ts
+++ src/services/userApi.ts
@@ -213,7 +213,7 @@ export const userApi = {
     try {
       console.log(`Blacklisting user: ${userId}`);
       // Call API to blacklist user
-      const response = await fetch(`http://localhost:4000/users/${userId}`, {
+      const response = await fetch(`http://localhost:4000/${userId}`, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
@@ -240,7 +240,7 @@ export const userApi = {
     try {
       console.log(`Activating user: ${userId}`);
       // Call API to activate user
-      const response = await fetch(`http://localhost:4000/users/${userId}`, {
+      const response = await fetch(`http://localhost:4000/${userId}`, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
